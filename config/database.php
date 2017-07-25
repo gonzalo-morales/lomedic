1afa9f8ad5e6375e9da47f687eb3265bcb161d0f
juanfrancolomedic <juan.franco@lomedic.com>
2017-07-25 08:59:19 -0500

    Modificacion 	Se modificaron las conexiones a la base de datos. 	Se modifica Migracion de tabla de usuarios, para especificar la conexion.

--- config/database.php
+++ config/database.php
@@ -32,21 +32,7 @@ return [
 	*/
 
 	'connections' => [
-		'pgsql' => [
-			'driver' => 'pgsql',
-			'host'   => env('DB_HOST', '127.0.0.1'),
-			'port'   => env('DB_PORT', '5432'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
-			'charset'  => 'utf8',
-		    'timezone' =>config('app.timezone'),
-			'prefix'   => '',
-			'schema'   => 'maestro',
-			'sslmode'   => 'prefer',
-		],
-	    
-	    'maestro' => [
+	    'corporativo' => [
 	        'driver' => 'pgsql',
 	        'host'   => env('DB_HOST', '127.0.0.1'),
 	        'port'   => env('DB_PORT', '5432'),
