7b91614d023b9658cca3ae4b460d7f6fdfc9cc6e
juanfrancolomedic <juan.franco@lomedic.com>
2017-07-25 08:59:19 -0500

    Modificacion 	Se modificaron las conexiones a la base de datos. 	Se modifica Migracion de tabla de usuarios, para especificar la conexion.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('ges_cat_usuarios', function (Blueprint $table) {
+        Schema::conection(config('database.conecction.corporativo.schema'))
+            ->create('ges_cat_usuarios', function (Blueprint $table) {
             /*Principal fields*/
             $table->increments('id_usuario');
             $table->string('usuario','20')->unique()->comment('Nombre de usuario');
             $table->string('nombre_corto','100')->unique()->comment('Nombre corto de la persona');
             $table->integer('fk_id_empleado')->comment('ID de empleado recursos humanos')->unsigned()->nullable();/*Foreign key from 'empleado' left*/
+            $table->integer('fk_id_empresa_default')->comment('ID de empresa default')->unsigned()->nullable();/*Foreign key from 'empresas' left*/
             /*$table->string('email')->unique();*/
             $table->string('password','60')->comment('Contrasena de acceso');
             $table->rememberToken();
@@ -26,6 +28,10 @@ class CreateUsersTable extends Migration
             /*General fields*/
             $table->boolean('activo')->default('1');
             $table->boolean('eliminar')->default('0');
+            
+            /*Foreign keys*/
+            $table->foreign('fk_id_empresa_default')->references('id_empresa')->on('gen_cat_empresas')->
+            onDelete('restrict')->onUpdate('restrict');
         });
     }
 
