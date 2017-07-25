dc41e736f804c35779e9583e2f491c576d2d4161
juanfrancolomedic <juan.franco@lomedic.com>
2017-07-25 09:24:28 -0500

    Modificacion: 	Se cambia la conexion de sesiones

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
 	|
 	*/
 
-	'default' => env('DB_CONNECTION', 'maestro'),
+	'default' => env('DB_CONNECTION', 'corporativo'),
 
 	/*
 	|--------------------------------------------------------------------------
