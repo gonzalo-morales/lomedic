66ffd6fea525ac292bb6d3fcecef152267ff323d
Héctor González <hectorgonzalezlomedic@abisalud.com>
2017-07-25 08:17:49 -0500

    Actualización de las migraciones existentes al nuevo modelo de logs y se agregó una conexión del schema logs en database.php

---
+++ database/migrations/2017_07_24_162636_create_permisos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermisosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ges_cat_permisos', function (Blueprint $table) {
+            $table->increments('id_permiso');
+            $table->string('descripcion');
+
+            /*General fields*/
+            $table->boolean('activo')->default('1');
+            $table->boolean('eliminar')->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ges_cat_permisos');
+    }
+}
