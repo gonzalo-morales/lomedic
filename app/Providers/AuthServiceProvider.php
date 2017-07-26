1e7d326a0541de26cd57d2781baa4a9aa3bc03d2
Hector Octavio Carrillo Becerra <hector.carrillo@lomedic.local>
2017-07-26 09:25:10 -0500

    _

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Http\Models\Administracion\Permisos;
+use App\Http\Models\Administracion\Empresas;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -21,10 +27,37 @@ class AuthServiceProvider extends ServiceProvider
 	 *
 	 * @return void
 	 */
-	public function boot()
+	public function boot(Permisos $permisos, Empresas $empresas)
 	{
 		$this->registerPolicies();
 
-		//
+
+		// $empresa = $empresas->where('conexion', 'corporativo')->first();
+
+		// if ($empresa) {
+		// 	dump( $empresa->modulos  );
+		// }
+
+
+		// Get the currently authenticated user...
+$user = Auth::user();
+
+
+// Get the currently authenticated user's ID...
+$id = Auth::id();
+
+
+dump( $user, $id );
+
+
+
+
+		// dump( $permisos->all() );
+		// Gate::define('some-permiso', function() {
+		// 	return $algo ? true : false;
+		// });
+
+
+
 	}
 }
