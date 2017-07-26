a731b5ebc1b41d443ae66e769ff2370733b0b97f
Héctor González <hector.gonzalez@abisalud.com>
2017-07-26 08:34:34 -0500

    Modificaciones en log

--- app/Http/Models/Logs.php
+++ app/Http/Models/Logs.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Http\Models\Administracion\Empresas;
+use App\Http\Models\AccionLog;
 
 class Logs extends Model
 {
@@ -14,12 +15,14 @@ class Logs extends Model
 
     protected $table = 'log_movimientos';
 
+    protected $primaryKey = 'id_log';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['tabla','fk_id_usuario', 'fk_id_empresa', 'accion','id_registro','fecha_hora','comentario'
+    protected $fillable = ['tabla','fk_id_usuario', 'fk_id_empresa', 'fk_id_accion','id_registro','fecha_hora','comentario'
         ,'ip_cliente','ip_servidor','dominio_servidor'];
 
     /**
@@ -28,16 +31,15 @@ class Logs extends Model
      * @var bool
      */
     public $timestamps = false;
-    public $primaryKey = false;
-    public $incrementing = false;
 
     public static function createLog($table,$created,$company)//Crear un registro en log cuando se agrega un registro a una tabla
-    {
+    {//Creación de un registro
         Logs::create([
+            'id_log' => '',
             'tabla' => $table,
             'fk_id_usuario' => Auth::id(),
             'fk_id_empresa' => Empresas::where('nombre_comercial',strtoupper($company))->first(['id_empresa'])->id_empresa,
-            'accion' =>'crear',
+            'fk_id_accion' =>1,
             'id_registro' => $created,
             'fecha_hora' => DB::raw('now()'),
             'comentario' => 'Registro insertado',
@@ -46,32 +48,21 @@ class Logs extends Model
             'dominio_servidor' => $_SERVER['SERVER_NAME']
         ]);
     }
-    public static function editLog($table,$company,$id)//Crear un registro en log cuando se modifica un registro a una tabla
-    {
-        Logs::create([
-            'tabla' => $table,
-            'fk_id_usuario' => Auth::id(),
-            'fk_id_empresa' => Empresas::where('nombre_comercial',strtoupper($company))->first(['id_empresa'])->id_empresa,
-            'accion' =>'editar',
-            'id_registro' => $id,
-            'fecha_hora' => DB::raw('now()'),
-            'comentario' => 'Registro editado',
-            'ip_cliente' => $_SERVER['REMOTE_ADDR'],
-            'ip_servidor' => $_SERVER['SERVER_ADDR'],
-            'dominio_servidor' => $_SERVER['SERVER_NAME']
-        ]);
-    }
+    public static function editLog($table,$company,$id,$accion,$comentario)//Crear un registro en log cuando se modifica un registro a una tabla
+    {//Editar, eliminar, mostrar e index
 
-    public static function deleteLog($table,$company,$id)//Crear un registro en log cuando se elimina un registro a una tabla
-    {
+
+
+
         Logs::create([
+            'id_log' => '',
             'tabla' => $table,
             'fk_id_usuario' => Auth::id(),
             'fk_id_empresa' => Empresas::where('nombre_comercial',strtoupper($company))->first(['id_empresa'])->id_empresa,
-            'accion' =>'eliminar',
+            'fk_id_accion' =>AccionLog::where('accion',$accion)->first(['id_accion'])->id_accion,
             'id_registro' => $id,
             'fecha_hora' => DB::raw('now()'),
-            'comentario' => 'Registro eliminado',
+            'comentario' => $comentario,
             'ip_cliente' => $_SERVER['REMOTE_ADDR'],
             'ip_servidor' => $_SERVER['SERVER_ADDR'],
             'dominio_servidor' => $_SERVER['SERVER_NAME']
