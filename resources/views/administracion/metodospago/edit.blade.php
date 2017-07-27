4f76eba79f21a5f1832821dc829d867d0e8869ff
Sergio De Dios <sergio.dedios@lomedic.com>
2017-07-27 09:47:03 -0500

    NUEVO: 		Parentescos	1.0.0	BD y Código 		Métodos de Pago	1.0.0	BD y Código 		Municipios	1.0.0	BD y Código 		Estados		1.0.0	BD y Código 		Países		1.0.0	BD y Código

---
+++ resources/views/administracion/metodospago/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Editar')
+
+@section('header-top')
+@endsection
+
+@section('header-bottom')
+	<script src="{{ asset('js/dataTableGeneralConfig.js') }}"></script>
+@endsection
+
+@section('content')
+<div class="col s12 xl8 offset-xl2">
+	<p class="left-align">
+		<a href="{{ url()->previous() }}" class="waves-effect waves-light btn">Regresar</a> <br>
+	</p>
+	<div class="divider"></div>
+</div>
+<div class="col s12 xl8 offset-xl2">
+	<h4>Editar {{ trans_choice('messages.'.$entity, 0) }}</h4>
+</div>
+
+<div class="col s12 xl8 offset-xl2">
+	<div class="row">
+		<form action="{{ companyRoute("update", ['company'=> $company, 'id' => $data->id_metodo_pago]) }}" method="post" class="col s12">
+			{{ csrf_field() }}
+			{{ method_field('PUT') }}
+			<div class="row">
+				<div class="input-field col s4">
+					<input type="text" name="metodo_pago" id="metodo_pago" class="validate" value="{{ $data->metodo_pago }}">
+					<label for="metodo_pago">Método de Pago</label>
+					@if ($errors->has('metodo_pago'))
+						<span class="help-block">
+							<strong>{{ $errors->first('metodo_pago') }}</strong>
+						</span>
+					@endif
+				</div>
+				<div class="input-field col s4">
+					<p>
+						@if ($data->activo == 1)
+						<input type="hidden" name="activo" value="0">
+						<input type="checkbox" id="activo"  name="activo" checked="true"/>
+						@else
+						<input type="hidden" name="activo" value="0">
+						<input type="checkbox" id="activo"  name="activo" />
+						@endif
+						<label for="activo">¿Activo?</label>
+					</p>
+
+					@if ($errors->has('activo'))
+					<span class="help-block">
+						<strong>{{ $errors->first('activo') }}</strong>
+					</span>
+					@endif
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input type="text" name="descripcion" id="descripcion" class="validate" value="{{ $data->descripcion }}">
+					<label for="descripcion">Descripción</label>
+					@if ($errors->has('descripcion'))
+						<span class="help-block">
+							<strong>{{ $errors->first('descripcion') }}</strong>
+						</span>
+					@endif
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<button class="waves-effect waves-light btn right">Guardar {{ trans_choice('messages.'.$entity, 0) }}</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+@endsection
