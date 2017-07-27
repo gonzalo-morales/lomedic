f6a6c6070ff62587e28728f2307917887da7a47a
Héctor González <hector.gonzalez@abisalud.com>
2017-07-27 08:07:29 -0500

    Validación de usuario para log. Migraciones varias.

--- database/migrations/2017_07_26_174217_alter_clave_producto_servicio_table.php
+++ database/migrations/2017_07_26_174217_alter_clave_producto_servicio_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVehiculoMarcaTable extends Migration
+class AlterClaveProductoServicioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,11 @@ class CreateVehiculoMarcaTable extends Migration
     public function up()
     {
         Schema::connection('corporativo')
-            ->create('gen_cat_vehiculos_marcas', function (Blueprint $table) {
-            $table->increments('id_marca');
-            $table->string('marca');
-            $table->boolean('activo')->default('true');
-            $table->boolean('eliminar')->default('false');
+            ->rename('sat_cat_clave_producto_servicio', 'sat_cat_claves_productos_servicios');
+
+        Schema::connection('corporativo')
+            ->table('sat_cat_clave_producto_servicio', function (Blueprint $table) {
+            //
         });
     }
 
@@ -30,6 +30,8 @@ class CreateVehiculoMarcaTable extends Migration
     public function down()
     {
         Schema::connection('corporativo')
-            ->dropIfExists('gen_cat_vehiculo_marca');
+            ->table('sat_cat_clave_producto_servicio', function (Blueprint $table) {
+            //
+        });
     }
 }
