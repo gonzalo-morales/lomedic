bbd8db2d6bb27f5f3dbf4629fae477068817b328
ihernandezt <israel.hernandez@lomedic.com>
2017-07-31 15:47:35 -0500

    Nuevo modelo y contralador
    Se agrego el modelo y el controlador del modulo de unidades de medida

---
+++ app/Http/Models/Administracion/Unidadesmedidas.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Models\Administracion;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unidadesmedidas extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'gen_cat_unidades_medidas';
+
+    /**
+     * The primary key of the table
+     * @var string
+     */
+    protected $primaryKey = 'id_unidad_medida';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre', 'activo'];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The validation rules
+     * @var array
+     */
+    public $rules = [
+
+        'nombre' => 'required',
+    ];
+
+    public function usuario()
+    {
+        $this->hasOne('app\Http\Models\Administracion\Usuarios');
+    }
+
+    public function empresa()
+    {
+        $this->$this->hasOne('app\Http\Models\Administracion\Empresas');
+    }
+}
