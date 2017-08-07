8eddb54cef30b74530f459659d851d3f55cbfd23
juanfrancolomedic <juan.franco@lomedic.com>
2017-08-07 15:42:45 -0500

    Se agregan error 403 y 404 Se agrega configuracion para laravel collective Se agregan rutas correctas para RH Se cambian las vistas de bancos Se agrega funcion para traer parametros default de un campo Se agrega configuracion de correo electronico

--- app/Http/Models/Administracion/Bancos.php
+++ app/Http/Models/Administracion/Bancos.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Models\Administracion;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Bancos extends Model
@@ -51,5 +52,24 @@ class Bancos extends Model
     public function numeroscuenta()
     {
         return $this->hasMany('App\Http\Models\Administracion\NumerosCuenta');
+    }
+    
+    public function ColumnDefaultValues()
+    {
+        $schema = config('database.connections.'.$this->getConnection()->getName().'.schema');
+
+        $data = DB::table('information_schema.columns')
+            ->select('column_name', 'data_type', DB::Raw("replace(replace(column_default, concat('::',data_type), ''),'''','') as column_default"))
+            ->whereRaw('column_default is not null')
+            ->whereRaw("column_default not ilike '%nextval%'")
+            ->where('table_name','=',$this->table)
+            ->where('table_schema','=',$schema)
+            ->where('table_catalog','=',$this->getConnection()->getDatabaseName())->get();
+        
+       foreach ($data as $value) {
+           $data->{$value->column_name} = $value->data_type == 'boolean' ? $value->column_default == 'true' : $value->column_default;
+       }
+        
+        return $data;
     }
 }
