1c1b0a1c9fdd1399b101b9e0844128360f27e3f3
juanfrancolomedic <juan.franco@lomedic.com>
2017-08-17 12:33:53 -0500

    Actualizacion Modelo usuario. Correccion en menu. Actualizacion Rutas de RH y Soporte

--- routes/RecursosHumanosRoute.php
+++ routes/RecursosHumanosRoute.php
@@ -17,11 +17,11 @@ Route::pattern('company', "($Conecctions)");
 
 Route::prefix('{company}')->group(function () {
     Route::group(['prefix' => 'recursos_humanos', 'as' => 'recursos_humanos.', 'middleware' => ['auth','share']], function(){
-        Route::resource('empleados', 'RecursosHumanos\EmpleadosController');
-        Route::resource('puestos', 'RecursosHumanos\PuestosController');
-        Route::resource('departamentos', 'RecursosHumanos\DepartamentosController');
         Route::resource('causasbajas', 'RecursosHumanos\CausasBajasController');
+        Route::resource('departamentos', 'RecursosHumanos\DepartamentosController');
+        Route::resource('empleados', 'RecursosHumanos\EmpleadosController');
         Route::get('empleadosautocomplete','RecursosHumanos\EmpleadosController@obtenerEmpleados');
         Route::get('empleado','RecursosHumanos\EmpleadosController@obtenerEmpleado');
+        Route::resource('puestos', 'RecursosHumanos\PuestosController');
     });
 });
