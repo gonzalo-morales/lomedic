aa98903b760d9e196a329fe6dfe6150991e7aee6
hernando-cabral <hernando.cabral@lomedic.com>
2017-09-20 15:14:56 -0500

    Nuevo
    Nuevas pantallas de login y admin, en este caso son pantallas meramente DEMOS, tienen integrado el js y css de bootstrap, también tiene popper.min.js que es un componente que copmplementa a bootstrap, sin embargo no es necesario intergarlo al smarth...

--- html/js/dataChart.js
+++ html/js/dataChart.js
@@ -1,39 +1,53 @@
-$(document).ready(function(){
-	var ctx = document.getElementById("myChart");
-	var myChart = new Chart(ctx, {
-	    type: 'bar', //tipo de chart
-	    data: {
-	        labels: ["IPEJAL", "SEGA", "Farmacia Rev", "GENE", "LoMedic", "TWITCH"],
-	        datasets: [{
-	            label: '$(k)',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
-	});
-});
+var chart = AmCharts.makeChart( "chartdiv", {
+  "type": "serial",
+  "theme": "light",
+  "dataProvider": [ {
+    "country": "100",
+    "visits": 2025
+  }, {
+    "country": "112",
+    "visits": 1882
+  }, {
+    "country": "124",
+    "visits": 1809
+  }, {
+    "country": "136",
+    "visits": 1322
+  }, {
+    "country": "148",
+    "visits": 1122
+  }, {
+    "country": "160",
+    "visits": 395
+  } ],
+  "valueAxes": [ {
+    "gridColor": "#FFFFFF",
+    "gridAlpha": 0.2,
+    "dashLength": 0
+  } ],
+  "gridAboveGraphs": true,
+  "startDuration": 1,
+  "graphs": [ {
+    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "fillAlphas": 0.8,
+    "lineAlpha": 0.2,
+    "type": "column",
+    "valueField": "visits"
+  } ],
+  "chartCursor": {
+    "categoryBalloonEnabled": false,
+    "cursorAlpha": 0,
+    "zoomable": false
+  },
+  "categoryField": "country",
+  "categoryAxis": {
+    "gridPosition": "start",
+    "gridAlpha": 0,
+    "tickPosition": "start",
+    "tickLength": 20
+  },
+  "export": {
+    "enabled": true
+  }
+
+} );
