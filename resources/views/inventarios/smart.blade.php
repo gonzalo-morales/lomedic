73f7b89483858f92ec19ea152f5f53aa2c00d24b
hernando-cabral <hernando.cabral@lomedic.com>
2017-09-22 09:54:33 -0500

    Cambios
    cambios en clases...

--- resources/views/inventarios/smart.blade.php
+++ resources/views/inventarios/smart.blade.php
@@ -21,32 +21,32 @@
 @section('form-content')
 {{ Form::setModel($data) }}
 <div class="row">
-	<div class="form-group col-md-4 col-xs-4">
+	<div class="form-group col-md-4 col-sm-4">
 		{!! Form::text('codigo',null, ['id'=>'codigo']) !!}
 		{{ Form::label('codigo', '* Código') }}
 		{{ $errors->has('codigo') ? HTML::tag('span', $errors->first('codigo'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="form-group col-md-4 col-xs-4">
+	<div class="form-group col-md-4 col-sm-4">
 		{!! Form::text('nombre',null,['id'=>'nombre]) !!}
 		{{ Form::label('nombre', '* Nombre') }}
 		{{ $errors->has('nombre') ? HTML::tag('span', $errors->first('nombre'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="form-group col-md-2 col-xs-2">
+	<div class="form-group col-md-2 col-sm-2">
 		{!! Form::text('fk_id_sucursal',null, ['id'=>'fk_id_sucursal','autocomplete'=>'off','data-url'=>companyAction('Administracion\SucursalesController@obtenerSucursales')]) !!}
 		{{ Form::label('fk_id_sucursal', '* Sucursal') }}
 		{{ $errors->has('fk_id_sucursal') ? HTML::tag('span', $errors->first('fk_id_sucursal'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="form-group col-md-2 col-xs-2">
+	<div class="form-group col-md-2 col-sm-2">
 		{!! Form::checkbox('virtual',1,true) !!}
 		{{ Form::label('virtual', '¿Es un almacen virtual?') }}
 	</div>
 	{{--Si la solicitud está cancelada--}}
 		@if(isset($data->fk_id_estatus_solicitud) && $data->fk_id_estatus_solicitud ==3)
-			<div class="form-group col-md-2 col-xs-2">
+			<div class="form-group col-md-2 col-sm-2">
 				{!! Form::text('fecha_cancelacion',$data->fecha_cancelacion,['disabled']) !!}
 				{{ Form::label('fecha_cancelacion','Fecha de cancelación') }}
 			</div>
-			<div class="form-group col col-md-10 col-xs-10">
+			<div class="form-group col col-md-10 col-sm-10">
 				{!! Form::text('motivo_cancelacion',$data->motivo_cancelacion,['disabled']) !!}
 				{{ Form::label('motivo_cancelacion','Motivo de la cancelación') }}
 			</div>
