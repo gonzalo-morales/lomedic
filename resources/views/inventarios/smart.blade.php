5c58bec41ccfbeaa1929afae5e30dc16b837f95a
hernando-cabral <hernando.cabral@lomedic.com>
2017-09-22 09:34:41 -0500

    cambio
    cambio en las clases del formulario.

--- resources/views/inventarios/smart.blade.php
+++ resources/views/inventarios/smart.blade.php
@@ -10,43 +10,43 @@
 @section('form-actions')
 	<div class="row">
 		<div class="right">
-			{{ Form::button('Guardar', ['type' =>'submit', 'class'=>'waves-effect waves-light btn orange']) }}
-			{!! HTML::decode(link_to(companyRoute('index'), 'Cerrar', ['class'=>'waves-effect waves-teal btn-flat teal-text'])) !!}
+			{{ Form::button('Guardar', ['type' =>'submit', 'class'=>'btn btn-primary']) }}
+			{!! HTML::decode(link_to(companyRoute('index'), 'Cerrar', ['class'=>'btn btn-default'])) !!}
 		</div>
 	</div>
 @endsection
 
-@section('content-width', 's12')
+@section('content-width', 'container-fluid')
 
 @section('form-content')
 {{ Form::setModel($data) }}
 <div class="row">
-	<div class="input-field col s4 m4">
+	<div class="form-group col-md-4 col-xs-4">
 		{!! Form::text('codigo',null, ['id'=>'codigo']) !!}
 		{{ Form::label('codigo', '* Código') }}
-		{{ $errors->has('codigo') ? HTML::tag('span', $errors->first('codigo'), ['class'=>'help-block deep-orange-text']) : '' }}
+		{{ $errors->has('codigo') ? HTML::tag('span', $errors->first('codigo'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="input-field col s4 m4">
+	<div class="form-group col-md-4 col-xs-4">
 		{!! Form::text('nombre',null,['id'=>'nombre]) !!}
 		{{ Form::label('nombre', '* Nombre') }}
-		{{ $errors->has('nombre') ? HTML::tag('span', $errors->first('nombre'), ['class'=>'help-block deep-orange-text']) : '' }}
+		{{ $errors->has('nombre') ? HTML::tag('span', $errors->first('nombre'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="input-field col s2 m2">
+	<div class="form-group col-md-2 col-xs-2">
 		{!! Form::text('fk_id_sucursal',null, ['id'=>'fk_id_sucursal','autocomplete'=>'off','data-url'=>companyAction('Administracion\SucursalesController@obtenerSucursales')]) !!}
 		{{ Form::label('fk_id_sucursal', '* Sucursal') }}
-		{{ $errors->has('fk_id_sucursal') ? HTML::tag('span', $errors->first('fk_id_sucursal'), ['class'=>'help-block deep-orange-text']) : '' }}
+		{{ $errors->has('fk_id_sucursal') ? HTML::tag('span', $errors->first('fk_id_sucursal'), ['class'=>'help-block text-danger']) : '' }}
 	</div>
-	<div class="input-field col s2 m2">
+	<div class="form-group col-md-2 col-xs-2">
 		{!! Form::checkbox('virtual',1,true) !!}
 		{{ Form::label('virtual', '¿Es un almacen virtual?') }}
 	</div>
 	{{--Si la solicitud está cancelada--}}
 		@if(isset($data->fk_id_estatus_solicitud) && $data->fk_id_estatus_solicitud ==3)
-			<div class="input-field col s2 m2">
+			<div class="form-group col-md-2 col-xs-2">
 				{!! Form::text('fecha_cancelacion',$data->fecha_cancelacion,['disabled']) !!}
 				{{ Form::label('fecha_cancelacion','Fecha de cancelación') }}
 			</div>
-			<div class="input-field col s10 m10">
+			<div class="form-group col col-md-10 col-xs-10">
 				{!! Form::text('motivo_cancelacion',$data->motivo_cancelacion,['disabled']) !!}
 				{{ Form::label('motivo_cancelacion','Motivo de la cancelación') }}
 			</div>
