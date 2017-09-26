2fe6e960aba5f783fd0a875b79c77d9ccad39e80
hernando-cabral <hernando.cabral@lomedic.com>
2017-09-26 09:51:01 -0500

    Cambio
    Cambio en el módulo, se agregó un estílo para complementar los btotones tipo check boxes

--- resources/views/administracion/grupoproductos/smart.blade.php
+++ resources/views/administracion/grupoproductos/smart.blade.php
@@ -4,36 +4,41 @@
 @section('form-content')
 {{ Form::setModel($data) }}
 <div class="row">
-	<div class="form-group col-md-6 col-xs-12">
-		{{ Form::label('descripcion', 'Descripcion del grupo') }}
-		{{ Form::text('descripcion', null, ['id'=>'descripcion','class'=>'form-control']) }}
-		{{ $errors->has('descripcion') ? HTML::tag('span', $errors->first('descripcion'), ['class'=>'help-block deep-orange-text']) : '' }}
+	<div  class="col-md-12 text-center">
+		<div class="form-check">
+			{{ Form::hidden('activo', 0) }}
+			{{ Form::checkbox('activo', 1, old('activo'), ['id'=>'activo']) }}
+			{{ Form::label('activo', 'Activo') }}
+		</div>
+		<div class="alert alert-warning" role="alert">
+		  Recuerda que al <b>inactivar</b> este catálogo no se mostrará en los modulos correspondientes que se requieran.
+		</div>
 	</div>
-	<div class="form-group col-md-6 col-xs-12">
-		{{ Form::label('descripcion_producto', 'Descripcion del producto') }}
-		{{ Form::text('descripcion_producto', null, ['id'=>'descripcion_producto','class'=>'form-control']) }}
-		{{ $errors->has('descripcion_producto') ? HTML::tag('span', $errors->first('descripcion_producto'), ['class'=>'help-block deep-orange-text']) : '' }}
-	</div>
-	<div class="col-md-12 col-xs-12">
-		{{ Form::label('nomenclatura', '* Nomenclatura') }}
-		<div class="input-group">
-			{{ Form::text('nomenclatura', null, ['id'=>'nomenclatura','class'=>'form-control']) }}
-			{{--{{ Form::label('tipo', 'Tipo') }}--}}
-			{{--{{ Form::text('tipo', null, ['id'=>'tipo','class'=>'form-control']) }}--}}
-			{{--{{ $errors->has('tipo') ? HTML::tag('span', $errors->first('tipo'), ['class'=>'help-block deep-orange-text']) : '' }}--}}
-			<div class="input-group-addon" role="group" aria-label="tipo" data-toggle="buttons">
-				<label class="btn btn-check btn-secondary">
-					<input type="radio" name="tipo" id="medicamento" autocomplete="off" value="1" class="btn btn-secondary">Medicamento
-				</label>
-				<label class="btn btn-check btn-secondary">
-					<input type="radio" name="tipo" id="material_curacion" autocomplete="off" value="2"  class="btn btn-secondary">Material de Curación
-				</label>
-			</div>
-			<div class="input-group-addon">
-				{{ Form::hidden('activo', 0) }}
-				{{ Form::checkbox('activo', 1, old('activo'), ['id'=>'activo']) }}
-				{{ Form::label('activo', 'Activo') }}
-			</div>
+		<div class="form-group col-md-5 col-xs-12">
+			{{ Form::label('descripcion', 'Descripcion del grupo') }}
+			{{ Form::text('descripcion', null, ['id'=>'descripcion','class'=>'form-control']) }}
+			{{ $errors->has('descripcion') ? HTML::tag('span', $errors->first('descripcion'), ['class'=>'help-block deep-orange-text']) : '' }}
+		</div>
+		<div class="form-group col-md-5 col-xs-12">
+			{{ Form::label('descripcion_producto', 'Descripcion del producto') }}
+			{{ Form::text('descripcion_producto', null, ['id'=>'descripcion_producto','class'=>'form-control']) }}
+			{{ $errors->has('descripcion_producto') ? HTML::tag('span', $errors->first('descripcion_producto'), ['class'=>'help-block deep-orange-text']) : '' }}
+		</div>
+		<div class="col-md-2 col-xs-12">
+			{{ Form::label('nomenclatura', '* Nomenclatura') }}
+				{{ Form::text('nomenclatura', null, ['id'=>'nomenclatura','class'=>'form-control']) }}
+				{{--{{ Form::label('tipo', 'Tipo') }}--}}
+				{{--{{ Form::text('tipo', null, ['id'=>'tipo','class'=>'form-control']) }}--}}
+				{{--{{ $errors->has('tipo') ? HTML::tag('span', $errors->first('tipo'), ['class'=>'help-block deep-orange-text']) : '' }}--}}
+		</div>
+	<div  class="col-md-12 text-center">
+		<div class="btn-group" data-toggle="buttons">
+			<label class="btn btn-check btn-secondary">
+				<input type="radio" name="tipo" id="medicamento" autocomplete="off" value="1" class="btn btn-secondary">Medicamento
+			</label>
+			<label class="btn btn-check btn-secondary">
+				<input type="radio" name="tipo" id="material_curacion" autocomplete="off" value="2"  class="btn btn-secondary">Material de Curación
+			</label>
 		</div>
 	</div>
 	{{ $errors->has('nomenclatura') ? HTML::tag('span', $errors->first('nomenclatura'), ['class'=>'help-block deep-orange-text']) : '' }}
