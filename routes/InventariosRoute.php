057d46c383a89e07c80fd5923a4f0135739ec12c
ihernandezt <israel.hernandez@lomedic.com>
2017-10-16 08:57:18 -0500

    Actualizacion de entrada de almacen

--- routes/InventariosRoute.php
+++ routes/InventariosRoute.php
@@ -23,5 +23,8 @@ Route::prefix('{company}')->group(function () {
         Route::get('getUpcs/{id}','Inventarios\ProductosController@obtenerUpcs');
         Route::resource('upcs','Inventarios\UpcsController');
         Route::resource('almacenes','Inventarios\AlmacenesController');
+        Route::resource('entradas','Inventarios\EntradasController');
+        Route::post('getOrdenes','Inventarios\EntradasController@getOrdenes')->name('entradas.getOrdenes');
+        Route::post('getDetalleOrden','Inventarios\EntradasController@getDetalleOrden')->name('entradas.getDetalleOrden');
     });
 });
