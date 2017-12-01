177fab31736cbcb46c6765ffc530626fcf75d3d3
juanfrancolomedic <juan.franco@lomedic.com>
2017-12-01 11:29:42 -0600

    correccion

--- public/js/app.js
+++ public/js/app.js
@@ -11,17 +11,17 @@ $(document).ready(function(){
        cursorborder: 'none'
    });
 // Inicializar los datepicker para las fechas necesarias
-$('.datepicker').pickadate({
+/*$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 3, // Creates a dropdown of 3 years to control year
     min: true,
     format: 'yyyy-mm-dd'
-});
+});*/
 
    /* Sidebar's */
    $('#sidebarCollapse').on('click', function () {
        $('#sidebar').toggleClass('active');
-       //condiciones para cambiar el ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â­cono de menÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Âº a x
+       //condiciones para cambiar el ÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Â­cono de menÃƒÆ’Ã†â€™Ãƒâ€ Ã¢â‚¬â„¢ÃƒÆ’Ã¢â‚¬Å¡Ãƒâ€šÃ‚Âº a x
        if($('#sidebar').hasClass('active') && $(window).width() >= 768){//Si es desktop
            $(this).find("i").text("menu");
        }
