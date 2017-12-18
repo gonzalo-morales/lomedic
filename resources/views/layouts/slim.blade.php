079a9a29c58f78a20e4c18aaf5de978271a3c27d
juanfrancolomedic <juan.franco@lomedic.com>
2017-12-18 15:44:27 -0600

    Correccion exportacion a excel

--- resources/views/layouts/slim.blade.php
+++ resources/views/layouts/slim.blade.php
@@ -3,28 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>SIM - @yield('title')</title>
+	<title>@yield('title')</title>
 	{{ HTML::meta('viewport', 'width=device-width, initial-scale=1') }}
 	{{ HTML::favicon(asset("img/logotipos/$menuempresa->logotipo")) }}
-	{{ HTML::style(asset('css/bootstrap/dist/css/bootstrap.min.css')) }}
 	{{ HTML::style(asset('css/style.css')) }}
-	
 </head>
 <body>
 <div class="black-text" style="max-width: 94%;">
 	@yield('content')
 </div>
-<!-- jQuery CDN -->
-{{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js') }}
-
-<!-- jQuery local fallback -->
-<script>window.jQuery || document.write('<script src="{{asset('js/jquery.min.js') }}"><\/script>')</script>
-
-{{ HTML::script('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js') }}
-{{ HTML::script(asset('js/popper.min.js')) }}
-
-<!-- Bootstrap JS CDN -->
-{{ HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js') }}
-{{ HTML::script(asset('js/bootstrap.min.js')) }}
 </body>
 </html>
