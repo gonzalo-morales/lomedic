dcf4aa3fa24f4f8518c042f93ee125a2720841c9
juanfrancolomedic <juan.franco@lomedic.com>
2017-12-19 12:04:51 -0600

    acomodo de vista para la exportacion de datos

--- resources/views/layouts/slim.blade.php
+++ resources/views/layouts/slim.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>@yield('title')</title>
-	{{ HTML::meta('viewport', 'width=device-width, initial-scale=1') }}
-	{{ HTML::favicon(asset("img/logotipos/$menuempresa->logotipo")) }}
-	{{ HTML::style(asset('css/style.css')) }}
-</head>
-<body>
-<div class="black-text" style="max-width: 94%;">
-	@yield('content')
-</div>
-</body>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="robots" content="noindex,nofollow" />
+    	<base href="{{url('/')}}" />
+    	<title>@yield('title')</title>
+    	{{ HTML::meta('viewport', 'width=device-width, initial-scale=1') }}
+    	{{ HTML::favicon(asset("img/logotipos/$menuempresa->logotipo")) }}
+    </head>
+    <body>
+    <div class="black-text" style="max-width: 100%;">
+    	@yield('content')
+    </div>
+    </body>
 </html>
