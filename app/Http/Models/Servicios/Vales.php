97c1ada90b76f710d7088974bde65f1b42d10f64
ihernandezt <israel.hernandez@lomedic.com>
2018-01-18 09:07:21 -0600

    Actualizacion vales

---
+++ app/Http/Models/Servicios/Vales.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Models\Servicios;
+
+use App\Http\Models\ModelCompany;
+use App\Http\Models\Administracion\Afiliaciones;
+use App\Http\Models\Administracion\Diagnosticos;
+use App\Http\Models\Administracion\Sucursales;
+use App\Http\Models\Administracion\Medicos;
+use App\Http\Models\Administracion\Programas;
+use App\Http\Models\Proyectos\Proyectos;
+
+class Vales extends ModelCompany
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'rec_opr_vales';
+
+    /**
+     * The primary key of the table
+     * @var string
+     */
+    protected $primaryKey = 'id_vale';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'observaciones',
+        'fk_id_usuario_captura',
+        'fk_id_usuario_modificacion',
+        'fecha_modificacion',
+        'fk_id_receta',
+        'fecha_surtido',
+        'cancelado',
+        'fecha_cancelado',
+        'motivo_cancelado',
+        'fk_id_usuario_cancelado',
+        'eliminado',
+        'fk_id_estatus_surtido',
+    ];
+
+    /**
+     * The validation rules
+     * @var array
+     */
+    public $rules = [
+//        'peso' => 'between:0,999.99',
+//        'altura' => 'between:0,9.99',
+//        'presion1' => 'between:0,999.99',
+//        'presion2' => 'between:0,999.99',
+//        'id_diagnostico' => 'required|numeric',
+//        'id_afiliacion' => 'required_without:nombre_paciente_no_afiliado',
+//        'nombre_paciente_no_afiliado' => 'required_without:id_afiliacion'
+    ];
+
+    /**
+     * Los atributos que seran visibles en index-datable
+     * @var array
+     */
+    protected $fields = [
+        'id_vale' => '#',
+        'fk_id_receta'=>'Receta',
+//        'observaciones'=>'Observaciones',
+//        'tipo_servicio' => 'Tipo de servicio',
+//        'id_afiliacion' => 'N. de afiliacion',
+//        'nombre_completo_paciente' => 'Paciente',
+//        'fecha_formated' => 'Fecha Captura',
+//        'estatus_formated' => 'Estatus de la receta'
+    ];
+
+    public function getNombreCompletoMedicoAttribute()
+    {
+        return $this->medico->nombre.' '.$this->medico->paterno.' '.$this->medico->materno;
+    }
+
+    public function getNombreCompletoPacienteAttribute()
+    {
+        if($this->id_afiliacion != '' && $this->id_afiliacion != null){
+            return $this->afiliacion->paterno.' '.$this->afiliacion->materno.' '.$this->afiliacion->nombre;
+        }else{
+            return $this->nombre_paciente_no_afiliado;
+        }
+    }
+
+    public function getTipoServicioAttribute(){
+        if($this->id_afiliacion != '' || $this->id_afiliacion != null){
+            return 'Afiliado';
+        }else{
+            return 'Externo';
+        }
+    }
+
+    public function getUnidadMedicaAttribute(){
+        return $this->sucursal->sucursal;
+    }
+
+    public function getFechaFormatedAttribute(){
+        return date("d-m-Y",strtotime($this->fecha));
+    }
+
+    public function getEstatusFormatedAttribute(){
+
+        return $this->estatus->estatus_receta;
+    }
+
+    public function afiliacion()
+    {
+        return $this->belongsTo(Afiliaciones::class,'fk_id_afiliacion','id_afiliacion');
+    }
+
+    public function diagnostico()
+    {
+        return $this->belongsTo(Diagnosticos::class,'fk_id_diagnostico','id_diagnostico');
+    }
+
+    public function sucursal()
+    {
+        return $this->belongsTo(Sucursales::class,'fk_id_sucursal','id_sucursal');
+    }
+
+    public function medico()
+    {
+        return $this->belongsTo(Medicos::class,'fk_id_medico','id_medico');
+    }
+
+    public function programa()
+    {
+        return $this->belongsTo(Programas::class,'fk_id_programa','id_programa');
+    }
+
+    public function estatus()
+    {
+        return $this->belongsTo(EstatusRecetas::class,'fk_id_estatus_receta','id_estatus_receta');
+    }
+    public function proyecto()
+    {
+        return $this->hasMany(Proyectos::class,'fk_id_proyecto','id_proyecto');
+    }
+    public function detalles()
+    {
+        return $this->hasMany( ValesDetalle::class,'fk_id_vale','id_vale');
+    }
+}
---
+++ public/js/vales.js
@@ -0,0 +1,92 @@
+/**
+ * Created by ihernandezt on 15/1/2018.
+ */
+
+$(document).ready(function () {
+
+
+    $('#fk_id_sucursal').select2();
+    // $('#id_receta').select2();
+    let token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+    $('#fk_id_sucursal').on('change', function() {
+        $.ajax({
+            type: "POST",
+            url: $(this).data('url'),
+            data: {'fk_id_sucursal':$(this).val(),'_token':token},
+            dataType: "json",
+            success:function(data) {
+                $('#fk_id_receta').empty();
+                $.each(data, function(key, value) {
+                    $('#fk_id_receta').append('<option value="'+ key +'">'+ value +'</option>');
+                });
+                $('#fk_id_receta').val('');
+            }
+        });
+    });
+    $('#fk_id_receta').on('change', function() {
+        if (!$(this).is(":empty")) {
+            $('#detalle tbody tr').remove();
+            $.ajax({
+                type: "POST",
+                url: $('#fk_id_receta').data('url'),
+                data: {'fk_id_receta':$(this).val(),'_token':token},
+                dataType: "json",
+                success:function(data) {
+                    // console.info(data);
+                    $.each(data, function(key,values) {
+                        $('#detalle').append(
+                            '<tr>' +
+                            '<td>'+values.clave_cliente_producto+'</td>'+
+                            '<td>'+values.descripcion+'</td>'+
+                            '<td>'+values.cantidad_solicitada+'</td>'+
+                            '<td class="cantidad_surtida">'+values.cantidad_surtida+'</td>'+
+                            '<td class="cantidad_disponible">'+values.cantidad_disponible+'</td>'+
+                            '<td><input type="number" onchange="calculatotal(this)" name="relations[has][detalles][' + key + '][cantidad_surtida]" min="0" max="'+(values.cantidad_solicitada - values.cantidad_surtida)+'" class="form-control cantidad" value="0"></td>'+
+                            '<td>$ '+parseFloat(values.precio_unitario, 10).toFixed(2)+'</td>'+
+                            '<td class="text-right total">$ '+parseFloat(0, 10).toFixed(2)+'</td>' +
+                            '<input type="hidden" class="cantidad_inicial_disponible" value="'+values.cantidad_disponible+'"/> ' +
+                            '<input type="hidden" name="relations[has][detalles][' + key + '][id_surtido_vale]"  value=""/> ' +
+                            '<input type="hidden" name="relations[has][detalles][' + key + '][fk_id_surtido_vale]"  value=""/> ' +
+                            '<input type="hidden" name="relations[has][detalles][' + key + '][fk_id_clave_cliente_producto]"  value="'+ values.fk_id_clave_cliente_producto +'"/> ' +
+                            '<input type="hidden" name="relations[has][detalles][' + key + '][cantidad_solicitada]"  value="'+ values.cantidad_solicitada +'"/> ' +
+                            '<input type="hidden" name="relations[has][detalles]['+ key +'][precio_unitario]" class="precio" value="'+ values.precio_unitario +'">'+
+                            '<input type="hidden" name="relations[has][detalles]['+ key +'][importe]" class="importe" value="'+ values.precio_unitario +'">'+
+                            '</tr>'
+                        );
+                    })
+                }
+            });
+        }
+    });
+});
+function calculatotal(el) {
+    var cantidad = $(el).val();
+    var precio = $(el).parent().parent().find('.precio').val();
+    var cantidad_surtida = $(el).parent().parent().find('.cantidad_surtida').html();
+    var cantidad_total = parseInt(cantidad_surtida)+parseInt(cantidad);
+    var cantidad_disponible = parseInt($(el).parent().parent().find('.cantidad_inicial_disponible').val());
+
+    console.info(cantidad_disponible+' '+cantidad);
+    if( (cantidad_disponible - cantidad) >= 0 )
+    {
+        var nueva_cantidad_diponible = cantidad_disponible - cantidad;
+    }
+    else if( (cantidad_disponible - cantidad) < 0 )
+    {
+        var nueva_cantidad_diponible = 0;
+        cantidad_total = cantidad_total - 1;
+        $(el).val(cantidad-1);
+    }
+
+    $(el).parent().parent().find('.cantidad_disponible').html(nueva_cantidad_diponible);
+    $(el).parent().parent().find('.importe').val(cantidad_total*precio);
+    $(el).parent().parent().find('.total').html('$ '+parseFloat((cantidad_total*precio), 10).toFixed(2));
+
+    var total =  0;
+    $('.importe').each(function (i) {
+        total += cantidad_total*precio;
+    });
+
+    $('#total').html('$ '+parseFloat(total, 10).toFixed(2));
+};
+
