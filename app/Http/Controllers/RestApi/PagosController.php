1ad023b2752817c5daabc2dafde973dca2e466e3
Héctor González <hectorgonzalezlomedic@abisalud.com>
2018-02-06 15:03:55 -0600

    RestApi

--- app/Http/Controllers/RestApi/PagosController.php
+++ app/Http/Controllers/RestApi/PagosController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\RestApi;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Models\Compras\Pagos;
 
 class PagosController extends Controller
 {
+    function __construct(Pagos $entity)
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        return response()->json(['status'=>'ok','data'=>Pagos::all()], 200);
+        return response()->json(['status'=>'ok','data'=>"INDEX"], 200);
+    }
+
+    public function show()
+    {
+        return response()->json(['status'=>'ok','data'=>"SHOW"], 200);
     }
 }
