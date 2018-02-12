dec15e375d973ca90987db51e048b99d8a526c3a
ihernandezt <israel.hernandez@lomedic.com>
2018-02-12 17:23:04 -0600

    Actualizacion afiliados

---
+++ public/js/afiliados.js
@@ -0,0 +1,52 @@
+/**
+ * Created by ihernandezt on 22/1/2018.
+ */
+
+var from_contrato = $('#fecha_nacimiento').pickadate({ selectMonths: true, selectYears: 3, format: 'yyyy-mm-dd' }).pickadate('picker');
+
+// $('#fk_id_afiliacion').on('change', function() {
+//
+//    if($('#fk_id_afiliacion').val() != '' )
+//    {
+//        $('#id_afiliacion').val($('#fk_id_afiliacion').val());
+//
+//        // let token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+//        //
+//        //     $.ajax({
+//        //         type: "POST",
+//        //         url: $(this).data('url'),
+//        //         data: {'fk_id_afiliacion':$(this).val(),'_token':token},
+//        //         dataType: "json",
+//        //         success:function(data) {
+//        //             $('.dependientes').empty();
+//        //             $.each(data, function(key, value) {
+//        //                  $('.dependientes').append('<tr>' +
+//        //                     '<td>'+value.id_afiliacion+'</td>' +
+//        //                     '<td>'+value.nombre+'</td>' +
+//        //                     '<td>'+value.genero+'</td>' +
+//        //                     '<td>'+value.parentesco+'</td>' +
+//        //                     '<td>'+value.fecha_nacimiento+'</td>' +
+//        //                     '</tr>');
+//        //             });
+//        //         }
+//        //     });
+//
+//    }
+//    // else
+//    // {
+//    //     $('#id_afiliacion').val('');
+//    // }
+//
+// });
+
+$('#fk_id_parentesco').on('change', function() {
+    $('#fk_id_afiliacion').prop("disabled", false);
+});
+
+$('#fk_id_afiliacion').on('change', function() {
+    $('#id_dependiente').val($('#fk_id_afiliacion').val());
+});
+
+$( "#form-model" ).submit(function( event ) {
+    $("#form-model :input").prop("disabled", false);
+});
--- resources/views/administracion/afiliados/smart.blade.php
+++ resources/views/administracion/afiliados/smart.blade.php
@@ -3,64 +3,30 @@
 
 
 @if (Route::currentRouteNamed(currentRouteName('index')))
-    @section('form-title', 'Pacientes')
+    @section('form-title', 'Afliados')
 @elseif(Route::currentRouteNamed(currentRouteName('create')))
-    @section('form-title', 'Nuevo paciente')
+    @section('form-title', 'Nuevo afiliado')
 @elseif(Route::currentRouteNamed(currentRouteName('edit')))
-    @section('form-title', 'Editar paciente')
+    @section('form-title', 'Editar afiliado')
 @elseif(Route::currentRouteNamed(currentRouteName('show')))
-    @section('form-title', 'Paciente')
+    @section('form-title', 'Afiliado')
 @endif
-
-
 
 @section('header-bottom')
     @parent
     @if (!Route::currentRouteNamed(currentRouteName('index')) && !Route::currentRouteNamed(currentRouteName('show')) )
-        <script type="text/javascript" src="{{ asset('js/pacientes.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/afiliados.js') }}"></script>
     @endif
 @endsection
 
 @section('form-content')
     {{ Form::setModel($data) }}
-
-    <div class="card z-depth-1-half">
-
-    @if (Route::currentRouteNamed(currentRouteName('create')))
+    <div class="card zz-depth-1-half">
     <div class="card-header">
         <div class="row">
             <div class="col-12 mb-3">
                 <div class="tab-content">
                     <div class="tab-pane active" role="tabpanel">
-                        <div class="row">
-
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                    {{ Form::cSelect('Paciente', 'fk_id_afiliacion', $afiliados ?? [],['class'=>'select2','data-url'=>companyRoute('getDependientes')]) }}
-                                </div>
-                            </div>
-                            <div class="col-sm-3">
-                                <div class="form-group">
-                                    {{ Form::cText('Numero de paciente', 'id_afiliacion',['class'=>'form-control'])}}
-                                </div>
-                            </div>
-                            <div class="col-sm-2">
-                                <div class="form-group">
-                                    {{ Form::cSelect('Parentesco', 'fk_id_parentesco', $parentescos ?? [],['class'=>'select2']) }}
-                                </div>
-                            </div>
-                            <div class="col-sm-1">
-                                <div class="form-group">
-                                    {{Form::cRadio('Genero','genero',['M'=>'Masculino','F'=>'Femenino'],['class'=>'form-control'])}}
-                                </div>
-                            </div>
-                            <div class="col-sm-2">
-                                <div class="form-group">
-                                    {{Form::cDate('Fecha de nacimiento','fecha_nacimiento',['class'=>' datepicker'])}}
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="row">
                             <div class="col-sm-3">
                                 <div class="form-group">
@@ -77,46 +43,42 @@
                                     {{ Form::cText('Apellido materno', 'materno',['class'=>'form-control']) }}
                                 </div>
                             </div>
+                            <div class="col-sm-1">
+                                <div class="form-group">
+                                    {{Form::cRadio('Genero','genero',['M'=>'Masculino','F'=>'Femenino'],['class'=>'form-control'])}}
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    {{Form::cDate('Fecha de nacimiento','fecha_nacimiento',['class'=>' datepicker'])}}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    {{ Form::cText('Numero de paciente', 'id_afiliacion',['class'=>'form-control'])}}
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    {{ Form::cSelect('Parentesco', 'fk_id_parentesco', $parentescos ?? [],['class'=>'select2']) }}
+                                </div>
+                            </div>
+                            @if (!Route::currentRouteNamed(currentRouteName('show')))
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    {{ Form::cSelect('Paciente', 'fk_id_afiliacion', $afiliados ?? [],['class'=>'select2','data-url'=>companyRoute('getDependientes'),'disabled']) }}
+                                </div>
+                            </div>
+                            @endif
+                            <input type="hidden" name="id_dependiente" id="id_dependiente" value="0">
                         </div>
                     </div>
                 </div>
             </div><!--/row-->
         </div>
     </div>
-    @endif
-    @if(!Route::currentRouteNamed(currentRouteName('index')))
-    <div class="card-body row table-responsive">
-        <table class="table highlight mt-3" id="tContactos">
-            <thead>
-            <tr>
-                <th>Numero de paciente</th>
-                <th>Nombre</th>
-                <th>Genero</th>
-                <th>Parentesco</th>
-                <th>Fecha de nacimiento</th>
-            </tr>
-            </thead>
-            <tbody class="dependientes">
-
-                @if(!Route::currentRouteNamed(currentRouteName('create')))
-                    @foreach($afiliados as $afiliado)
-                        <tr>
-                            <td>{{$afiliado['id_afiliacion']}}</td>
-                            <td>{{$afiliado['nombre'].' '.$afiliado['paterno'].' '.$afiliado['materno']}}</td>
-                            <td>{{$afiliado['genero']}}</td>
-                            <td>{{$afiliado->parentesco->nombre}}</td>
-                            <td>{{$afiliado->fecha_nacimiento}}</td>
-                        </tr>
-                     @endforeach
-                @endif
-            </tbody>
-        </table>
-
     </div>
-    @endif
-</div>
-
-
-
 @endsection
 
