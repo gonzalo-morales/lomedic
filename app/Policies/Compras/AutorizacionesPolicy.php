afe6e023c836a66e70d9132944616714f63842e1
juanfrancolomedic <juan.franco@lomedic.com>
2018-03-05 09:45:19 -0600

    Correccion Autorizaciones

--- app/Policies/Compras/AutorizacionesPolicy.php
+++ app/Policies/Compras/AutorizacionesPolicy.php
@@ -3,8 +3,12 @@
 namespace App\Policies\Compras;
 
 use App\Policies\PolicyBase;
+use App\Http\Models\Administracion\Usuarios;
 
 class AutorizacionesPolicy extends PolicyBase
 {
-
+    public function create(Usuarios $usuario)
+    {
+        return false;
+    }
 }
--- resources/views/compras/autorizaciones/smart.blade.php
+++ resources/views/compras/autorizaciones/smart.blade.php
@@ -16,33 +16,16 @@
     			<h3>Autorización de la {{$data->fk_id_tipo_documento == 10 ? 'solicitud de pago' : 'orden de compra'}} No. {{$data->fk_id_documento}}</h3>
     		</div>
     	</div>
+        <div class="row">
+        	<div class="form-group col-md-4 col-sm-6">
+        		{{Form::cText('* Tipo de autorizacion','',isset($data) && !empty($data->condicion) ? $data->condicion->nombre : '',['class'=>'form-control','disabled'])}}
+        	</div>
+        	<div class="form-group col-md-8 col-sm-6">
+        		{{Form::cRadio('* ¿Autorizado?','fk_id_estatus',[4=>'Autorizado',3=>'No Autorizado'])}}
+        	</div>
+        	<div class="form-group col-md-12 col-sm-12">
+        		{{Form::cTextArea('Motivo','observaciones',['readonly','style'=>'resize:none;'])}}
+        	</div>
+        </div>
     @endif
-    <div class="row">
-    	<div class="form-group col-md-4 col-sm-6">
-    		{{Form::cText('* Tipo de autorizacion','',isset($data) && !empty($data->condicion) ? $data->condicion->nombre : '',['class'=>'form-control','disabled'])}}
-    	</div>
-    	<div class="form-group col-md-8 col-sm-6">
-    		{{Form::cRadio('* ¿Autorizado?','fk_id_estatus',[4=>'Autorizado',3=>'No Autorizado'])}}
-    	</div>
-    	<div class="form-group col-md-12 col-sm-12">
-    		{{Form::cTextArea('Motivo','observaciones',['readonly','style'=>'resize:none;'])}}
-    	</div>
-    </div>
 @endsection
-
-{{-- DONT DELETE --}}
-@index
-	@section('form-title','Autorizaciones')
-@endif
-
-@crear
-	@section('form-title','Agregar Autorizacion')
-@endif
-
-@editar
-	@section('form-title','Autorizar')
-@endif
-
-@ver
-	@section('form-title','Autorizacion')
-@endif
