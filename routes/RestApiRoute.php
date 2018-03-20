ad9ed7702b9a621158889b41df0776e79f8581cf
juanfrancolomedic <juan.franco@lomedic.com>
2018-03-20 14:39:01 -0600

    Cambios en rutas para automatizacion del registro de controladores.

--- routes/RestApiRoute.php
+++ routes/RestApiRoute.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ |--------------------------------------------------------------------------
+ | Rest Api Routes
+ |--------------------------------------------------------------------------
+ | Here is where you can register web routes for your application. These
+ | routes are loaded by the RouteServiceProvider within a group which
+ | contains the "web" middleware group. Now create something great!
+ */
 
 $Conecctions = implode('|',array_keys(config('database.connections')));
 
@@ -6,14 +14,10 @@ Route::pattern('company', "($Conecctions)");
 
 Route::prefix('{company}')->group(function () {
     Route::group(['prefix' => 'restapi', 'as' => 'restapi.', 'middleware' => 'auth.api'], function() {
-        Route::resource('pagos', 'RestApi\PagosController');
-        Route::resource('facturasclientes', 'RestApi\FacturasClientesController');
-        Route::resource('facturasproveedores', 'RestApi\FacturasProveedoresController');
-        Route::resource('notascreditoclientes', 'RestApi\NotasCreditoClientesController');
-        Route::resource('notascargoclientes', 'RestApi\NotasCargoClientesController');
-        Route::resource('notascreditoproveedores', 'RestApi\NotasCreditoClientesController');
-        Route::resource('entradas', 'RestApi\MovimientosEntradaController');
-        Route::resource('salidas', 'RestApi\MovimientosSalidaController');
-        Route::resource('proveedores', 'RestApi\SociosNegocioController');
+        collect(\File::glob(app_path().'/Http/Controllers/RestApi/*Controller.php'))->map(function($file) {
+            $name = strtolower(substr(basename($file),0,-14));
+            $controller = basename(dirname($file)).'\\'.substr(basename($file),0,-4);
+            Route::resource($name,$controller);
+        });
     });
 });
--- routes/SoporteRoute.php
+++ routes/SoporteRoute.php
@@ -1,14 +1,11 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Soporte Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 
 $Conecctions = implode('|',array_keys(config('database.connections')));
@@ -17,13 +14,6 @@ Route::pattern('company', "($Conecctions)");
 
 Route::prefix('{company}')->group(function () {
     Route::group(['prefix' => 'soporte', 'as' => 'soporte.', 'middleware' => ['auth','share','csrf','password_expired']], function(){
-        Route::resource('acciones', 'Soporte\AccionesController');
-        Route::resource('categorias', 'Soporte\CategoriasController');
-        Route::resource('estatustickets', 'Soporte\EstatusTicketsController');
-        Route::resource('impactos', 'Soporte\ImpactosController');
-        Route::resource('modoscontacto', 'Soporte\ModosContactoController');
-        Route::resource('prioridades', 'Soporte\PrioridadesController');
-        Route::resource('seguimiento', 'Soporte\SeguimientoSolicitudesController');
         Route::get('solicitudes/asignadas', 'Soporte\SolicitudesController@index_tecnico')->name('solicitudes.index_tecnico');
         Route::get('solicitudes/disponibles', 'Soporte\SolicitudesController@index_tecnicos')->name('solicitudes.index_tecnicos');
         Route::get('solicitudes/{id}/acciones', 'Soporte\SolicitudesController@obtenerAcciones');
@@ -31,8 +21,11 @@ Route::prefix('{company}')->group(function () {
         Route::get('solicitudes/{id}/subcategorias', 'Soporte\SolicitudesController@obtenerSubcategorias');
         Route::get('solicitudes/getcategorias', 'Soporte\SolicitudesController@getCategorias');
         Route::get('solicitudes/getprioridades', 'Soporte\SolicitudesController@getPrioridades');
-        Route::resource('solicitudes', 'Soporte\SolicitudesController');
-        Route::resource('subcategorias', 'Soporte\SubcategoriasController');
-        Route::resource('urgencias', 'Soporte\UrgenciasController');
+        
+        collect(\File::glob(app_path().'/Http/Controllers/Soporte/*Controller.php'))->map(function($file) {
+            $name = strtolower(substr(basename($file),0,-14));
+            $controller = basename(dirname($file)).'\\'.substr(basename($file),0,-4);
+            Route::resource($name,$controller);
+        });
     });
 });
