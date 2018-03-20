6d7ed472f35f499cd153d71ed36aaf34b0d21ae9
juanfrancolomedic <juan.franco@lomedic.com>
2018-03-20 12:34:04 -0600

    Correccion modelo series

--- app/Http/Models/Administracion/SeriesDocumentos.php
+++ app/Http/Models/Administracion/SeriesDocumentos.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Models\Administracion;
 
-use App\Http\Models\ModelCompany;
+use App\Http\Models\ModelBase;
 
-class SeriesDocumentos extends ModelCompany
+class SeriesDocumentos extends ModelBase
 {
 	/**
 	 * The table associated with the model.
