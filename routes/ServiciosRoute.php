8aadd6b7b569c7c6ff504352b53e906748d8d4bc
ihernandezt <israel.hernandez@lomedic.com>
2018-03-20 15:43:14 -0600

    Actualizacion recetas

--- routes/ServiciosRoute.php
+++ routes/ServiciosRoute.php
@@ -1,13 +1,15 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
-| Servicios Routes
+| Web Routes
 |--------------------------------------------------------------------------
+|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+|
 */
-
 $Conecctions = implode('|',array_keys(config('database.connections')));
 
 Route::pattern('company', "($Conecctions)");
@@ -15,7 +17,6 @@ Route::pattern('company', "($Conecctions)");
 Route::prefix('{company}')->group(function () {
     Route::group(['prefix' => 'servicios', 'as' => 'servicios.', 'middleware' => ['auth','share','csrf','password_expired'] ], function() {
         Route::view("/","servicios.index");
-        
 	    Route::post('getAfiliados','Servicios\RecetasController@getAfiliados')->name('recetas.getAfiliados');
         Route::post('getProyectos','Servicios\RecetasController@getProyectos')->name('recetas.getProyectos');
 	    Route::post('getDiagnosticos','Servicios\RecetasController@getDiagnosticos')->name('recetas.getDiagnosticos');
@@ -25,13 +26,9 @@ Route::prefix('{company}')->group(function () {
 	    Route::get('recetas/{id}/surtirReceta','Servicios\RecetasController@surtirReceta')->name('recetas.surtirReceta');
 	    Route::post('recetas/{id}/surtir','Servicios\RecetasController@surtir')->name('recetas.surtir');
 	    Route::get('recetas/{id}/imprimirReceta','Servicios\RecetasController@imprimirReceta')->name('recetas.imprimirReceta');
-        
-	    collect(\File::glob(app_path().'/Http/Controllers/Servicios/*Controller.php'))->map(function($file) {
-	        $name = strtolower(substr(basename($file),0,-14));
-	        $controller = basename(dirname($file)).'\\'.substr(basename($file),0,-4);
-	        Route::resource($name,$controller);
-	    });
-        
+        Route::resource('recetas', 'Servicios\RecetasController');
+        Route::resource('requisicioneshospitalarias', 'Servicios\RequisicionesHospitalariasController');
+        Route::resource('vales', 'Servicios\ValesController');
         Route::post('verifyStock','Servicios\RecetasController@verifyStock')->name('recetas.verifyStock');
         Route::post('vales/getReceta','Servicios\ValesController@getReceta')->name('vales.getReceta');
         Route::post('vales/getRecetaDetalle','Servicios\ValesController@getRecetaDetalle')->name('vales.getRecetaDetalle');
@@ -39,5 +36,7 @@ Route::prefix('{company}')->group(function () {
         Route::post('requisicioneshospitalarias/getMedicamentos','Servicios\RecetasController@getMedicamentos')->name('requisicioneshospitalarias.getMedicamentos');
 //        Route::post('vales/impress','Servicios\ValesController@impress')->name('vales.impress');
         Route::get('vales/{id}/impress', 'Servicios\ValesController@impress')->name('vales');
+        Route::get('recetas/{id}/impress', 'Servicios\RecetasController@impress')->name('recetas');
+
     });
 });
