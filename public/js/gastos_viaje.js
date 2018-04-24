4e020cfd00938c0f79ad51d7c3d1eeb9152a30d4
hernando-cabral <hernando.cabral@lomedic.com>
2018-04-24 17:48:33 -0500

    Actualización Gastos Viaje
    Se cambiaron las funciones debido a la nueva API

--- public/js/gastos_viaje.js
+++ public/js/gastos_viaje.js
@@ -2,7 +2,20 @@ $(document).ready(function () {
 
   //Iniciamos los selects, fechas y tooltips
   initSelect2();
-  initDates();
+
+  $("#periodo_inicio").pickadate('picker').on({
+    set: function(){
+      $('#periodo_fin').pickadate('picker').set('min', $('#periodo_inicio').pickadate('picker').get('select'));
+      ingresarDias();
+    }
+  });
+
+  $("#periodo_fin").pickadate('picker').on({
+    set: function(){
+      ingresarDias();
+    }
+  });
+
   $('[data-toggle]').tooltip();
 
   //Funciones para ingresar valores de acuerdeo al empleado seleccionado (puesto, departamento, sucursal)
@@ -79,84 +92,6 @@ $(document).ready(function () {
     calcular();
   });
 
-  //FUNCIÓN PARA INICIAR LOS SELECTS
-  function initSelect2(){
-    $('#fk_id_tipo').select2({
-      placeholder: "Seleccione el concepto de la nota",
-      allowClear: true
-    });
-    $('#fk_id_empleado.select2').select2({
-      placeholder: "Seleccione el empleado",
-      allowClear: true
-    });
-  };
-
-  // FUNCIÓN PARA INICIAR LAS FECHAS Y SUS RESPECTIVAS FUNCIONES
-  function initDates(){
-    $('#fecha').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 3, // Creates a dropdown of 3 years to control year
-      min: true,
-      format: 'yyyy-mm-dd'
-    });
-
-    //Función para calcular días
-    $("#periodo_inicio").pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 3, // Creates a dropdown of 3 years to control year
-      min: false,
-      format: 'yyyy-mm-dd',
-      //Función para indicar que la segunda fecha tome el valor de la primera
-      onSet: function () {
-        $('#periodo_fin').pickadate('picker').set('min', $('#periodo_inicio').pickadate('picker').get('select'));
-      },
-    });
-
-    $("#periodo_fin").pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 3, // Creates a dropdown of 3 years to control year
-      min: false,
-      format: 'yyyy-mm-dd',
-      onSet: function () {
-        //Corremos la función para ingresar los días
-        ingresarDias();
-      }
-    });
-
-    //Función para ingresar y calcular los días de acuerdo a las dos fechas seleccionadas
-    function ingresarDias(){
-      var fecha1 = $('#periodo_inicio').pickadate('picker');
-      var fecha2 = $('#periodo_fin').pickadate('picker');
-      var datoFinal = $('#total_dias');
-      var fTotal = "";
-
-      var start= fecha1.get('select', 'yyyy/mm/dd');
-      var end= fecha2.get('select', 'yyyy/mm/dd');
-
-      start = new Date(start).getTime()
-      end = new Date(end).getTime()
-
-      var fTotal = end - start;
-      var days = Math.floor(fTotal / (1000 * 60 * 60 * 24));
-
-      //Función para indicar que la segunda fecha tome el valor de la primera
-      fecha1.on('set', function(event) {
-        if ( 'select' in event ) {
-          fecha2.start().clear().set('min', fecha1.get('select'));
-        }
-        if ( 'clear' in event ) {
-          fecha2.clear().set('min', false).stop();
-          $('#periodo_fin').prop('readonly', true);
-        }
-      });
-      //Condición para ingresar el total
-      if(days >= 0)
-        datoFinal.val(days);
-      else
-        datoFinal.val("N/A");
-    };
-  };
-
   $(document).on('submit', function(e){
     if($('#detalle-form-body > tr').length == 0){
       e.preventDefault();
@@ -195,98 +130,91 @@ $(document).ready(function () {
     }
     // limpiamos el formulario
     sumarTodosLosTotales();
-  });
-
-  //FUNCIÓN PARA INGRESAR VALORES A LA TABLA
-  function agregarFilaDetalle(){
-  var tableData = $('table > tbody');
-  var i = $('#detalle-form-body > tr').length;
-  var row_id = i > 0 ? +$('#detalle-form-body > tr:last').find('#index').val()+1 : 0;
-  var tipo = $('#fk_id_tipo option:selected').text();
-  var impuesto = $('#fk_id_impuesto option:selected').text();
-  //Ingresamos estos datos en la tabla
-  tableData.append(
-    '<tr><td>'+ returnArray.folio_fac + '<input type="hidden" id="index" value="'+row_id+'"></input><input type="hidden" name="relations[has][detalle]['+row_id+'][folio]" value="'+returnArray.folio_fac+'"/>'+'</td>'
-    + '<td>'+ tipo + '<input type="hidden" name="relations[has][detalle]['+row_id+'][fk_id_tipo]" value="'+returnArray.fk_id_tipo+'"/>'+'</td>'
-    + '<td>'+ '$' +returnArray.subtotal_fac + '<input class="subtotal" type="hidden" name="relations[has][detalle]['+row_id+'][subtotal]" value="'+returnArray.subtotal_fac+'"/>'+'</td>'
-    + '<td>'+ impuesto + '<input type="hidden" name="relations[has][detalle]['+row_id+'][fk_id_impuesto]" value="'+returnArray.fk_id_impuesto+'"/>'+'</td>'
-    + '<td>'+ '$' +returnArray.total_fac + '<input class="total" type="hidden" name="relations[has][detalle]['+row_id+'][total]" value="'+returnArray.total_fac+'"/>'+'</td>'
-    + '<td>'+ '<button data-toggle="Eliminar" data-placement="top" title="Eliminar" data-original-title="Eliminar" type="button" class="text-primary btn btn_tables is-icon eliminar bg-white" data-delay="50" onclick="borrarFila(this)"><i class="material-icons">delete</i></button>'+'</td></tr>'
-    );
-    $.toaster({priority : 'success',title : '¡Éxito!',message : 'Factura/Nota agregada con éxito',
-      settings:{'timeout':1000,'toaster':{'css':{'top':'5em'}}}
-    });
-  };
-
-  //FUNCIÓN PARA VALIDAR
-  function validateDetail() {
-    $('#folio_fac').rules('add',{
-      required: true,
-      maxlength: 15,
-      messages:{
-        required: 'Ingresa el número de folio o número de la nota'
-      }
-    });
-    $('#fk_id_tipo').rules('add',{
-      required: true,
-      messages:{
-        required: 'Es necesario seleccionar el concepto'
-      }
-    });
-    $('#fk_id_impuesto').rules('add',{
-      required: true,
-      messages:{
-        required: 'Es necesario seleccionar el tipo de impuesto'
-      }
-    });
-    $.validator.addMethod('subtotal',function (value,element) {
-        return this.optional(element) || /^\d{0,6}(\.\d{0,2})?$/g.test(value);
-    },'El subtotal no tiene un formato´válido');
-    $.validator.addMethod( "greaterThan", function( value, element, param ) {
-
-      if ( this.settings.onfocusout ) {
-          $(element).addClass( "validate-greaterThan-blur" ).on( "blur.validate-greaterThan", function() {
-              $( element ).valid();
-          } );
-      }
-
-        return value > param;
-    }, "Ingresa un valor mayor a 0" );
-    $('#subtotal_fac').rules('add',{
-        required: true,
-        number: true,
-        subtotal:true,
-        greaterThan:0,
-        messages:{
-            required: 'Para continuar es necesario un subtotal',
-            number: 'El campo debe ser un número',
-            greaterThan: 'El número debe ser mayor a 0',
-        }
-    });
-  };
-
-  //FUNCIÓN PARA LIMPIAR LOS CAMPOS
-  function limpiarCampos() {
-    initSelect2();
-    $('#fk_id_impuesto').val(0)
-    $('#folio_fac').val('');
-    $('#subtotal_fac').val('');
-    $('#total_fac').val('');
-    $('#impuesto').val(0);
-    //Eliminar reglas de validación detalle
-    $('#fk_id_tipo').rules('remove');
-    $('#fk_id_impuesto').rules('remove');
-    $('#folio_fac').rules('remove');
-    $('#fk_id_impuesto').rules('remove');
-    $('#subtotal_fac').rules('remove');
-  };
-
+  }); 
 });
 
-//FUNCIÓN PARA LIMPIAR LA FILA
+function agregarFilaDetalle(){
+var tableData = $('table > tbody');
+var i = $('#detalle-form-body > tr').length;
+var row_id = i > 0 ? +$('#detalle-form-body > tr:last').find('#index').val()+1 : 0;
+var tipo = $('#fk_id_tipo option:selected').text();
+var impuesto = $('#fk_id_impuesto option:selected').text();
+tableData.append(
+  '<tr><td>'+ returnArray.folio_fac + '<input type="hidden" id="index" value="'+row_id+'"></input><input type="hidden" name="relations[has][detalle]['+row_id+'][folio]" value="'+returnArray.folio_fac+'"/>'+'</td>'
+  + '<td>'+ tipo + '<input type="hidden" name="relations[has][detalle]['+row_id+'][fk_id_tipo]" value="'+returnArray.fk_id_tipo+'"/>'+'</td>'
+  + '<td>'+ '$' +returnArray.subtotal_fac + '<input class="subtotal" type="hidden" name="relations[has][detalle]['+row_id+'][subtotal]" value="'+returnArray.subtotal_fac+'"/>'+'</td>'
+  + '<td>'+ impuesto + '<input type="hidden" name="relations[has][detalle]['+row_id+'][fk_id_impuesto]" value="'+returnArray.fk_id_impuesto+'"/>'+'</td>'
+  + '<td>'+ '$' +returnArray.total_fac + '<input class="total" type="hidden" name="relations[has][detalle]['+row_id+'][total]" value="'+returnArray.total_fac+'"/>'+'</td>'
+  + '<td>'+ '<button data-toggle="Eliminar" data-placement="top" title="Eliminar" data-original-title="Eliminar" type="button" class="text-primary btn btn_tables is-icon eliminar bg-white" data-delay="50" onclick="borrarFila(this)"><i class="material-icons">delete</i></button>'+'</td></tr>'
+  );
+  $.toaster({priority : 'success',title : '¡Éxito!',message : 'Factura/Nota agregada con éxito',
+    settings:{'timeout':1000,'toaster':{'css':{'top':'5em'}}}
+  });
+};
+
+function validateDetail() {
+  $('#folio_fac').rules('add',{
+    required: true,
+    maxlength: 15,
+    messages:{
+      required: 'Ingresa el número de folio o número de la nota'
+    }
+  });
+  $('#fk_id_tipo').rules('add',{
+    required: true,
+    messages:{
+      required: 'Es necesario seleccionar el concepto'
+    }
+  });
+  $('#fk_id_impuesto').rules('add',{
+    required: true,
+    messages:{
+      required: 'Es necesario seleccionar el tipo de impuesto'
+    }
+  });
+  $.validator.addMethod('subtotal',function (value,element) {
+      return this.optional(element) || /^\d{0,6}(\.\d{0,2})?$/g.test(value);
+  },'El subtotal no tiene un formato´válido');
+  $.validator.addMethod( "greaterThan", function( value, element, param ) {
+
+    if ( this.settings.onfocusout ) {
+        $(element).addClass( "validate-greaterThan-blur" ).on( "blur.validate-greaterThan", function() {
+            $( element ).valid();
+        } );
+    }
+
+      return value > param;
+  }, "Ingresa un valor mayor a 0" );
+  $('#subtotal_fac').rules('add',{
+      required: true,
+      number: true,
+      subtotal:true,
+      greaterThan:0,
+      messages:{
+          required: 'Para continuar es necesario un subtotal',
+          number: 'El campo debe ser un número',
+          greaterThan: 'El número debe ser mayor a 0',
+      }
+  });
+};
+
+function limpiarCampos() {
+  initSelect2();
+  $('#fk_id_impuesto').val(0)
+  $('#folio_fac').val('');
+  $('#subtotal_fac').val('');
+  $('#total_fac').val('');
+  $('#impuesto').val(0);
+  //Eliminar reglas de validación detalle
+  $('#fk_id_tipo').rules('remove');
+  $('#fk_id_impuesto').rules('remove');
+  $('#folio_fac').rules('remove');
+  $('#fk_id_impuesto').rules('remove');
+  $('#subtotal_fac').rules('remove');
+};
+
 function borrarFila(el) {
   var tr = $(el).closest('tr');
-  //Volvemos a calular el total y subtotal general de todos los totales y subtotales en el detalle
   var trTotal = $(el).parent().parent().find('input.total').val();
   var trSubtotal = $(el).parent().parent().find('input.subtotal').val();
   var hiddenTotal = $('#total_detalles').val();
@@ -303,8 +231,6 @@ function borrarFila(el) {
       settings:{'timeout':1000,'toaster':{'css':{'top':'5em'}}}});
 };
 
-
-//FUNCIÓN PARA CALULAR Y SUMAR TODO (TOTALES DE DETALLE)
 function sumarTodosLosTotales(){
   var tdTotal = $('#detalle-form-body > tr > td > input.total');
   var tdSubtotal = $('#detalle-form-body > tr > td > input.subtotal');
@@ -321,7 +247,6 @@ function sumarTodosLosTotales(){
   $('#subtotal_detalles').val(subtotal.toFixed(2));
 }
 
-//FUNCIÓN PARA CALCULAR EL TOTAL
 function calcular(){
   var subtotal = +$('#subtotal_fac').val();
   var impuesto = +$('#impuesto').val();
@@ -329,3 +254,46 @@ function calcular(){
   var total = subtotal + totalImpuesto
   $('#total_fac').val(total.toFixed(2));
 }
+
+function initSelect2(){
+  $('#fk_id_tipo').select2({
+    placeholder: "Seleccione el concepto de la nota",
+    allowClear: true
+  });
+  $('#fk_id_empleado.select2').select2({
+    placeholder: "Seleccione el empleado",
+    allowClear: true
+  });
+};
+
+function ingresarDias(){
+  var fecha1 = $('#periodo_inicio').pickadate('picker');
+  var fecha2 = $('#periodo_fin').pickadate('picker');
+  var datoFinal = $('#total_dias');
+  var fTotal = "";
+
+  var start= fecha1.get('select', 'yyyy/mm/dd');
+  var end= fecha2.get('select', 'yyyy/mm/dd');
+
+  start = new Date(start).getTime()
+  end = new Date(end).getTime()
+
+  var fTotal = end - start;
+  var days = Math.floor(fTotal / (1000 * 60 * 60 * 24));
+
+  //Función para indicar que la segunda fecha tome el valor de la primera
+  fecha1.on('set', function(event) {
+    if ( 'select' in event ) {
+      fecha2.start().clear().set('min', fecha1.get('select'));
+    }
+    if ( 'clear' in event ) {
+      fecha2.clear().set('min', false).stop();
+      $('#periodo_fin').prop('readonly', true);
+    }
+  });
+  //Condición para ingresar el total
+  if(days >= 0)
+    datoFinal.val(days);
+  else
+    datoFinal.val("N/A");
+};
